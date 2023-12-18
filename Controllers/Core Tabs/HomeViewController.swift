aa215877e34ddab35feef934df50da7b173b49f9
Danylo <danilovityk@gmail.com>
2023-12-17 20:00:22 -0500

    inital commit

---
+++ Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Danik on 17.12.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
