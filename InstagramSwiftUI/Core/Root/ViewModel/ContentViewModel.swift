2986f4048561199e570d97b06e6c3beafd674578
Danylo <danilovityk@gmail.com>
2024-02-29 12:50:38 -0500

    add post view, firebase started

---
+++ InstagramSwiftUI/Core/Root/ViewModel/ContentViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentViewModel.swift
+//  InstagramSwiftUI
+//
+//  Created by Danik on 29.02.2024.
+//
+
+import Foundation
---
+++ InstagramSwiftUI/Core/UploadPost/UploadPostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UploadPostViewModel.swift
+//  InstagramSwiftUI
+//
+//  Created by Danik on 28.02.2024.
+//
+
+import Foundation
